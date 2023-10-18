737da8fae4fad032f27fa07dffd615bf1816bd76
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-10-18 14:09:58 +0800

    added routes for reference

--- SuperplusHero/src/App.vue
+++ SuperplusHero/src/App.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
     <router-link :to="{ name: 'Register' }">Register</router-link> |
+    <router-link :to="{ name: 'Login' }">LogIn</router-link> |
     <router-link :to="{ name: 'Home' }">Home</router-link> |
-    <router-link :to="{ name: 'Profile' }">Profile</router-link> |
-    <router-link :to="{ name: 'Login' }">Login</router-link>
+    <router-link :to="{ name: 'Profile' }">Profile</router-link>
   </div>
-
-  <p>
-  <button @click="$router.go(-1)">Go back 1 step</button>
-  <button @click="$router.go(1)" >Go forward 1 step</button>
-  </p>
-
   <router-view/>
 </template>
