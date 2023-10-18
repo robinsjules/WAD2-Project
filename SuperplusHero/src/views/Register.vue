7cbcb6955163aa56170b050373976c9b42933f6a
chester <chesterwong.2022@scis.smu.edu.sg>
2023-10-18 13:56:32 +0800

    login page

--- SuperplusHero/src/views/Register.vue
+++ SuperplusHero/src/views/Register.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- <h2>Register Page</h2> -->
+    <!-- <h2>Register Page</h2> --> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <!-- Section: Design Block -->
     <section class="">
