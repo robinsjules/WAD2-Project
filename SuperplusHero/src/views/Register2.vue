0195a5f018d06d2d2943afbfba2465e46f7c4d2e
chester <chesterwong.2022@scis.smu.edu.sg>
2023-10-22 17:29:59 +0800

    added some code in register2

--- SuperplusHero/src/views/Register2.vue
+++ SuperplusHero/src/views/Register2.vue
@@ -1,3 +1,37 @@
-<template>
-    
+<template>  
+    <div class="container">
+        <form>
+        
+            <div class="form-group">
+                <label for="allergies">Allergies</label>
+                <input type="text" class="form-control" id="allergies" placeholder="Enter your allergies">
+            </div>
+            <div class="form-group">
+                <label for="fridgeItems">Items in Fridge</label>
+                <input type="text" class="form-control" id="fridgeItems" placeholder="Enter items in your fridge">
+                
+                <button type="button" class="btn btn-success" @click="addItem()">Add Item</button>
+                <button type="button" class="btn btn-danger" @click="deleteItem()">Delete Item</button>
+                <script>
+                    export default {
+                        data() {
+                            return {
+                                items: []
+                            }
+                        },
+                        methods: {
+                            addItem() {
+                                let item = document.getElementById('fridgeItems').value;
+                                this.items.push(item);
+                            },
+                            deleteItem() {
+                                this.items.pop();
+                            }
+                        }
+                    }
+                </script>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 </template>
