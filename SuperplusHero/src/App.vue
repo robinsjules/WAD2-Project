77e773ecab58a6f8587ab3663bc91a6553b086d4
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-10-25 13:16:52 +0800

    Added logo

--- SuperplusHero/src/App.vue
+++ SuperplusHero/src/App.vue
@@ -1,10 +1,3 @@
 <template>
-  <div>
-    <router-link :to="{ name: 'Register' }">Register</router-link> |
-    <router-link :to="{ name: 'Login' }">LogIn</router-link> |
-    <router-link :to="{ name: 'Home' }">Home</router-link> |
-    <router-link :to="{ name: 'Profile' }">Profile</router-link>
-    <router-link :to="{ name: 'Register2' }">Register2</router-link>
-  </div>
   <router-view/>
 </template>
