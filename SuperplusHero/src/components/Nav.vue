35533d353b3f11ffe1c0523cdccef986d465c615
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-10-25 14:00:13 +0800

    removed link in nav oops

--- SuperplusHero/src/components/Nav.vue
+++ SuperplusHero/src/components/Nav.vue
@@ -1,27 +1,26 @@
 <template>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <nav v-if = "showNavBar" class="navbar navbar-expand-lg navbar-success bg-success   ">
-            <a class="navbar-brand" href="#"><img src="" alt="Logo" width="80" height="30"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="../assets/comm.png" alt="Community" width="50" height="50"></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="../assets/cart.png" alt="Cart" width="50" height="50"></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="../assets/fridge.png" alt="Fridge" width="50" height="50"></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><img src="../assets/profile.png" alt="Profile" width="50" height="50"></a>
-                    </li>
-                </ul>
-            </div>
-    </nav>
+  <nav v-if="showNavBar" class="navbar navbar-expand-lg navbar-success bg-success">
+    <a class="navbar-brand" href="#"><img src="" alt="Logo" width="80" height="30"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="../assets/comm.png" alt="Community" width="50" height="50"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="../assets/cart.png" alt="Cart" width="50" height="50"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="../assets/fridge.png" alt="Fridge" width="50" height="50"></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><img src="../assets/profile.png" alt="Profile" width="50" height="50"></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 <script>
 export default {
