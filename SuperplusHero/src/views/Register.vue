e7fda5e240bdf9ecd5432733e8fcf68b8dd79bbb
chester <chesterwong.2022@scis.smu.edu.sg>
2023-10-25 14:15:41 +0800

    made register2 look nicer

--- SuperplusHero/src/views/Register.vue
+++ SuperplusHero/src/views/Register.vue
@@ -1,6 +1,5 @@
 <template>
     <!-- <h2>Register Page</h2> --> 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <!-- Section: Design Block -->
     <section class="">
         <!-- Jumbotron -->
@@ -25,34 +24,34 @@
                                     <div class="row">
                                         <div class="col-md-6 mb-4">
                                             <div class="form-outline">
-                                                <input type="text" id="form3Example1" class="form-control" />
-                                                <label class="form-label" for="form3Example1">First name</label>
+                                                <input type="text" id="form3Example1" class="form-control" placeholder="First Name" />
+                                                <!-- <label class="form-label" for="form3Example1">First name</label> -->
                                             </div>
                                         </div>
                                         <div class="col-md-6 mb-4">
                                             <div class="form-outline">
-                                                <input type="text" id="form3Example2" class="form-control" />
-                                                <label class="form-label" for="form3Example2">Last name</label>
+                                                <input type="text" id="form3Example2" class="form-control" placeholder="Last Name" />
+                                                <!-- <label class="form-label" for="form3Example2">Last name</label> -->
                                             </div>
                                         </div>
                                     </div>
 
                                     <!-- Email input -->
                                     <div class="form-outline mb-4">
-                                        <input type="email" id="form3Example3" class="form-control" />
-                                        <label class="form-label" for="form3Example3">Email address</label>
+                                        <input type="email" id="form3Example3" class="form-control" placeholder="Email Address"/>
+                                        <!-- <label class="form-label" for="form3Example3">Email address</label> -->
                                     </div>
 
                                     <!-- Phone Number -->
                                     <div class="form-outline mb-4">
-                                        <input type="phonenum" id="form3Example3" class="form-control" />
-                                        <label class="form-label" for="form3Example3">Phone Number</label>
+                                        <input type="phonenum" id="form3Example3" class="form-control" placeholder="Phone Number"/>
+                                        <!-- <label class="form-label" for="form3Example3">Phone Number</label> -->
                                     </div>
 
                                     <!-- Password input -->
                                     <div class="form-outline mb-4">
-                                        <input type="password" id="form3Example4" class="form-control" />
-                                        <label class="form-label" for="form3Example4">Password</label>
+                                        <input type="password" id="form3Example4" class="form-control" placeholder="Password"/>
+                                        <!-- <label class="form-label" for="form3Example4">Password</label> -->
                                     </div>
 
                                     <!-- Checkbox -->
@@ -99,13 +98,12 @@
     </section>
     <!-- Section: Design Block -->
 </template>
-<script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous"></script>
 <script>
 export default {
   // Your component data and methods go here
   methods: {
     goToNext() {
-      this.$router.push({ name: 'Register2' });
+    this.$router.push({ name: 'Register2' });
     }
   }
 }
