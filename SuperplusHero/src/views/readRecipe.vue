bb8d691f4c82226c61d8d2abd67476f07eed15bf
benedictchia14 <mechia.2022@smu.edu.sg>
2023-10-26 08:46:10 +0800

    readRecipe

--- SuperplusHero/src/views/readRecipe.vue
+++ SuperplusHero/src/views/readRecipe.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="container-fluid mt-3">
+
+        <!-- Recipe Prep: Start -->
         <h2>Your Selected Recipe</h2>
         <div class="mt-4 p-4 bg-light text-black rounded">
             <div class="row">
@@ -15,6 +17,7 @@
                         <li>Remove the heads and shells from prawns and set aside in a bowl, then devein and set the prawn meat aside in a separate bowl.</li>
                         <li>To make the prawn stock, add cooking oil to a large saucepan on medium-high heat. Add prawn heads and shells then stir for 5 minutes, crushing the heads to release the flavour.</li>
                         <li>Add water and chicken stock, then simmer for 45 minutes allowing the stock to reduce.</li>
+                        .....
                     </ol>
 
                     <div style="text-align: right;"> <!-- Custom inline style to align the button to the right -->
@@ -22,11 +25,88 @@
                     </div>
                 </div>
             </div>
-            <!-- BS card: End -->
-            <h1>Singapore Laksa</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..
-            </p>
+            <!-- Recipe Prep: End -->
+
         </div>
-    </div>
+    
+
+            <h1>Required Ingridients (Serving Size: 4)</h1>
+
+            <div class="row">
+                
+            <!-- Green Box -->
+                <div class="col-md-6">
+                    <div class="mt-4 p-4 text-black rounded" style="background-color: rgb(226, 246, 244);">
+                        <h2>Available Ingredients:</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..
+                        </p>
+                        <ul class>
+                            <li class="fw-bold list-unstyled">Laksa Spice Base Paste</li>
+                            
+                            <li>100ml of Dried Chilli Paste or 10 Dried Chillies, soaked in boiling water</li> 
+                            <li>5 Red Chillies, chopped</li>
+                            <li>1 medium Onion or 2-3 shallots</li>
+                            <li>2-3 Garlic Cloves</li>
+                            <li>4 Candlenuts (or use Hazelnuts/Cashew Nuts/Macadamia)</li>
+                            <li>20g or 1 tbsp Ginger, chopped</li>
+                            <li>20g or 1 tbsp Galangal, chopped</li>
+                            <li>1 tsp Turmeric Powder or 20g or 1 tbsp Fresh Turmeric</li>
+                            <li>2 Lemongrass Stalks, inner white core</li>
+                            <li>20g or 1 tbsp Belachan or Fermented Dried Shrimp Paste</li>
+                            <li>30g or 1 tbsp Dried Shrimps</li>
+                            <li>1 tbsp Coriander Seeds</li>
+                            
+                            <br>
+                            <li class="fw-bold list-unstyled">Easy Prawn Stock</li>
+                                <li>300g Prawn heads and shells</li> 
+                                <li>1 litre Water </li>
+
+                            
+                            
+                            </ul>
+
+Laksa Gravy
+1 litre Prawn Broth
+
+300ml Coconut Milk
+
+2 tbsps Fish Sauce
+
+1 tbsp Brown Sugar, or to taste
+
+Fried Tofu Puffs or Taopok
+
+Laksa Leaves/Vietnamese Coriander Leaves/Daun Kesum (optional)
+
+Noodles and Other Ingredients
+Thick Vermicelli / Laksa Rice Noodles / Bee Tai Mak
+
+12 Prawns
+
+Fishcakes, sliced
+
+Blood Cockles (i did not use this)
+
+Beansprouts
+
+Bok Choy or other Asian Greens, blanched (optional)
+
+Boiled Eggs
+                    </div> 
+                </div>
+            <!-- Green Box -->
+
+            <!-- Red Box -->
+                <div class="col-md-6">
+                    <div class="mt-4 p-4 text-black rounded" style="background-color: rgb(246, 226, 226);">
+                        <h2>Missing Ingredients:</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..
+                        </p>
+                    </div> 
+                </div>
+            <!-- Red Box -->
+            </div>
+        </div>
 </template>
