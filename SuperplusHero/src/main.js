2db6ba1dd14db63e1fe73d2e05ad94660f70e399
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-10-27 19:03:16 +0800

    fixed axios in main.js

--- SuperplusHero/src/main.js
+++ SuperplusHero/src/main.js
@@ -3,8 +3,7 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-import "axios"
-
+import axios from "axios"
 
 const app = createApp(App)
 
