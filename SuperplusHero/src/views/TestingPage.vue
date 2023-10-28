7eb156504f32460888b28e24843baa30723f0d09
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-10-28 16:37:39 +0800

    testing

--- SuperplusHero/src/views/TestingPage.vue
+++ SuperplusHero/src/views/TestingPage.vue
@@ -1,32 +1,90 @@
 <template>
-    <div>
-      <h1>User List</h1>
-      <ul>
-        <li v-for="user in users" :key="user.email">{{ user.User_FName }}</li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  // import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        users: [],
+  <div>
+    <h2>Edit Profile</h2>
+    <form @submit.prevent="updateProfile">
+      <label for="Email">Email:</label>
+      <input type="text" id="Email" v-model="email" />
+
+      <label for="UserName">Username:</label>
+      <input type="text" id="UserName" v-model="userName" />
+
+      <label for="Password">Password:</label>
+      <input type="password" id="Password" v-model="password" />
+
+      <label for="PostalCode">Postal Code:</label>
+      <input type="text" id="PostalCode" v-model="postalCode" />
+
+      <label for="Halal">Halal:</label>
+      <input type="checkbox" id="Halal" v-model="halal" />
+
+      <label for="Fridge">Fridge:</label>
+      <textarea id="Fridge" v-model="fridge"></textarea>
+
+      <label for="Savings">Savings:</label>
+      <input type="number" id="Savings" v-model="savings" />
+
+      <label for="RecipesPosted">Recipes Posted:</label>
+      <input type="number" id="RecipesPosted" v-model="recipesPosted" />
+
+      <button type="submit">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      email: '', // Initialize with user's current email
+      userName: '', // Initialize with user's current username
+      password: '', // Initialize with user's current password
+      postalCode: '', // Initialize with user's current postal code
+      halal: false, // Initialize with user's current halal status
+      fridge: '', // Initialize with user's current fridge content (as JSON string)
+      savings: 0, // Initialize with user's current savings
+      recipesPosted: 0 // Initialize with user's current number of recipes posted
+    };
+  },
+  methods: {
+    updateProfile() {
+      // Construct the updated user data
+      const updatedData = {
+        userName: this.userName,
+        password: this.password,
+        postalCode: this.postalCode,
+        halal: this.halal,
+        fridge: JSON.parse(this.fridge), // Assuming you want to store the fridge data as JSON
+        savings: this.savings,
+        recipesPosted: this.recipesPosted
+        // Add other fields to be updated
       };
-    },
-    mounted() {
-      // Make an HTTP GET request to your Flask API
-      axios.get('http://127.0.0.1:5000/users') // Adjust the URL if needed
-        .then((response) => {
-          this.users = response.data;
-          console.log(this.users);
+      
+      // Make an HTTP PUT request to update the profile data
+      axios.put(`http://127.0.0.1:5000/update_profile/${this.userName}`, updatedData)
+        .then(response => {
+          console.log(response.data); // Handle success response
         })
-        .catch((error) => {
-          console.error('Error fetching data:', error);
+        .catch(error => {
+          console.error('Error updating data:', error); // Handle error
         });
-    },
-  };
-  </script>
-  
+    }
+    // mounted() {
+    //   // Fetch user's initial profile data (if needed)
+    //   // You may use this block to pre-fill the form with user's current data
+    //   // Make an HTTP GET request to fetch user data to pre-fill the form
+    //   axios.get(`http://127.0.0.1:5000/get_profile/${this.email}`)
+    //     .then(response => {
+    //       // Update the data properties with the received user data
+    //       this.email = response.data.email;
+    //       this.phone = response.data.phone;
+    //       // Update other fields similarly
+    //     })
+    //     .catch(error => {
+    //       console.error('Error fetching data:', error); // Handle error
+    //     });
+    // }
+  }
+};
+</script>
--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from supabase import create_client
 
@@ -9,10 +9,52 @@ url = "https://mpdbdcytohoflyionlyn.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wZGJkY3l0b2hvZmx5aW9ubHluIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc2MTAyNTIsImV4cCI6MjAxMzE4NjI1Mn0.1tKz3KYVupIppWbWLx3HMKi87CnZlrpqk1Ehht-Rb6c"
 supabase = create_client(url, key)
 
+# code works
 @app.route("/users", methods=['GET'])
 def getAllUsers():
     response = supabase.table('Users').select('*').execute()
     return(response.data)
+
+# code in progress -> Add users for when they register
+@app.route("/add_users", methods=['POST'])
+def addUsers():
+    try:
+        data = request.json  # Assuming the client sends a JSON object with user data
+
+        # Insert data into the Users table
+        response = supabase.table('Users').upsert(data, ['Email'])  # Assuming 'Email' is the primary key
+
+        if response['status_code'] == 200:
+            return jsonify({"message": "User added successfully"})
+        else:
+            return jsonify({"error": "Failed to add user"})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+# code works   
+@app.route('/get_profile/<username>', methods=['GET'])
+def get_profile(username):
+    print(f"Received request for username: {username}")
+    response = supabase.table('Users').select('*').eq('UserName', username).execute()
+    return (response.data)
+    
+# code in progress -> Route to update user data by username
+@app.route('/update_profile/<username>', methods=['POST','PUT'])
+def update_user(userName):
+    try:
+        data = request.json  # Assuming the client sends a JSON object with updated user data
+
+        # Update data in the Users table based on the specified email
+        response = supabase.table('Users').update(data, ['UserName'], condition=f'UserName.eq.{userName}')
+
+        if response['status_code'] == 200:
+            return jsonify({"message": "User updated successfully"})
+        else:
+            return jsonify({"error": "Failed to update user"})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
