345d8fbfb8b05d7a085a52f7b8ae7bd96d4876dc
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-11-01 16:30:24 +0800

    commented to do list for community page

--- SuperplusHero/src/views/Community.vue
+++ SuperplusHero/src/views/Community.vue
@@ -1,56 +1,17 @@
-<template>
-    <section class="content">
-        <div class="container posts-content">
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="card mb-4">
-                        <div class="card-body">
-                            <div class="media mb-3">
-                                <img src="https://bootdey.com/img/Content/avatar/avatar3.png"
-                                    class="d-block ui-w-40 rounded-circle" alt="">
-                                <div class="media-body ml-3">
-                                    Kenneth Frazier
-                                    <div class="text-muted small">3 days ago</div>
-                                </div>
-                            </div>
-
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus commodo bibendum.
-                                Vivamus laoreet blandit odio, vel finibus quam dictum ut.
-                            </p>
-                            <a href="" class="ui-rect ui-bg-cover"
-                                style="background-image: url('https://bootdey.com/img/Content/avatar/avatar3.png');"></a>
-                        </div>
-                        <div class="card-footer">
-                            <a href="#" class="d-inline-block text-muted like-button" @click="incrementCount">
-                                <small class="align-middle">
-                                    <strong class="like-count">{{ count + 109 }}</strong> Likes
-                                </small>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      count: 0
-    }
-  },
-  methods: {
-    incrementCount() {
-      this.count++;
-    }
-  }
-}
-</script>
-
 <style>
+/*
+Edits to be made:
+1. ensure that the page saves the like counter even after you reload (best if code knows if the logged in user has liked the photo already or not)
+2. show image based off the bucket in Supabase
+3. time stamp, username, title, desc to be taken from Supabase
+4. populate cards based off how many rows there are in Supabase
+5. include a filter by cuisine function (good to have)
+6. include a sorting of most liked
+7. include search function of hashtagging (good to have)
+8. reduce image size of the card, put the image of user beside the name instead
+9. redirect to posting page
+*/
+
 .content {
     margin-top: 100px;
 }
@@ -124,3 +85,55 @@ export default {
     border: 1px solid rgba(24, 28, 33, 0.06);
 }
 </style>
+
+<template>
+    <section class="content">
+        <div class="container posts-content">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <div class="media mb-3">
+                                <img src="https://bootdey.com/img/Content/avatar/avatar3.png"
+                                    class="d-block ui-w-40 rounded-circle" alt="">
+                                <div class="media-body ml-3">
+                                    Kenneth Frazier
+                                    <div class="text-muted small">3 days ago</div>
+                                </div>
+                            </div>
+
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus finibus commodo bibendum.
+                                Vivamus laoreet blandit odio, vel finibus quam dictum ut.
+                            </p>
+                            <a href="" class="ui-rect ui-bg-cover"
+                                style="background-image: url('https://bootdey.com/img/Content/avatar/avatar3.png');"></a>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="d-inline-block text-muted like-button" @click="incrementCount">
+                                <small class="align-middle">
+                                    <strong class="like-count">{{ count + 109 }}</strong> Likes
+                                </small>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+    }
+  }
+}
+</script>
