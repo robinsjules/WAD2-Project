caf940612b5c6d61f8af5a381049b25688373531
Sierra-Lima <sean.liew.2022@scis.smu.edu.sg>
2023-11-03 14:48:32 +0800

    Testing proxies

--- SuperplusHero/vite.config.js
+++ SuperplusHero/vite.config.js
@@ -13,6 +13,19 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-   
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/api2': {
+        target: 'http:127.0.0.1:5000/get_profile/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api2/, ''),
+      },
+    },
+  },
+});
 
