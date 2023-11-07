e7288dc884f69e9aca4cbe20d0213d010f329f4b
chester <chesterwong.2022@scis.smu.edu.sg>
2023-11-07 21:16:14 +0800

    removed login page template and reverted register page back

--- SuperplusHero/src/views/Register.vue
+++ SuperplusHero/src/views/Register.vue
@@ -2,63 +2,112 @@
 .btn {
     text-align: center;
 };
-@import url('https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-@import url('https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap');
-@import url('/login-form-20/css/style.css');
+@import url(../);
 </style>
 <template>
     <!-- Section: Design Block -->
-    <section class="ftco-section">
-		<div class="container" style="background-image: url('/login-form-20/images/bg.jpg');">
-			<div class="row justify-content-center">
-			</div>
-			<div class="row justify-content-center">
-				<div class="col-md-6 col-lg-4">
-					<div class="login-wrap p-0">
-						<h3 class="mb-4 text-center">Have an account?</h3>
-						<form action="#" class="signin-form">
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Username" required>
-							</div>
-							<div class="form-group">
-								<input id="password-field" type="password" class="form-control" placeholder="Password"
-									required>
-								<span toggle="#password-field"
-									class="fa fa-fw fa-eye field-icon toggle-password"></span>
-							</div>
-							<div class="form-group">
-								<button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
-							</div>
-							<div class="form-group d-md-flex">
-								<div class="w-50">
-									<label class="checkbox-wrap checkbox-primary">Remember Me
-										<input type="checkbox" checked>
-										<span class="checkmark"></span>
-									</label>
-								</div>
-								<div class="w-50 text-md-right">
-									<a href="#" style="color: #fff">Forgot Password</a>
-								</div>
-							</div>
-						</form>
-						<p class="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
-						<div class="social d-flex text-center">
-							<a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span>
-								Facebook</a>
-							<a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span>
-								Twitter</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+    <section class="">
+        <!-- Jumbotron -->
+        <div class="px-5 py-5 px-md-5 text-center text-lg-start"
+            style="background-color: hsl(0, 0%, 96%);background-size: cover; height: 115vh; background-image: url('https://www.europenowjournal.org/wp-content/uploads/2019/04/shutterstock_321864554.jpg');">
+            <div class="container">
+                <div class="row gx-lg-5 align-items-center">
+                    <div class="col-lg-6 mb-5 mb-lg-0">
+                        <h1 class="my-5 display-3 fw-bold ls-tight">
+                            Welcome to <br />
+                            <span class="text-success">Surplus Hero.</span>
+                        </h1>
+                        <p style="color: hsl(217, 10%, 50.8%)">
+                            The one stop platform for surplus groceries and recipe generation!
+                        </p>
+                    </div>
+
+                    <div class="col-lg-6 mb-5 mb-lg-0">
+                        <div class="card">
+                            <div class="card-body py-5 px-md-5">
+                                <div class=" fw-bolder ls-tight">
+                                    <h1 style="text-align: center;">Sign Up</h1>
+                                </div><br>
+
+                                <form @submit.prevent = "goToNext">
+                                    <!-- 2 column grid layout with text inputs for the first and last names -->
+                                    <div class="col-md-12 mb-4">
+                                        <div class="form-outline">
+                                            <select v-model="form.UserType" class="form-control" id="dropdown">
+                                                <option value="What type of user are you?" selected>What type of user are
+                                                    you?</option>
+                                                <option value="Consumer">Consumer</option>
+                                                <option value="Business">Business</option>
+
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 mb-4">
+                                        <div class="form-outline">
+                                            <input v-model="form.UserName" type="text" id="form3Example1"
+                                                class="form-control" placeholder="Username" />
+                                            <!-- <label class="form-label" for="form3Example1">First name</label> -->
+                                        </div>
+                                    </div>
+
+
+                                    <!-- Email input -->
+                                    <div class="form-outline mb-4">
+                                        <input v-model="form.Email" type="email" id="form3Example3" class="form-control"
+                                            placeholder="Email Address" />
+                                        <!-- <label class="form-label" for="form3Example3">Email address</label> -->
+                                    </div>
+
+                                    <!-- Phone Number -->
+                                    <div class="form-outline mb-4">
+                                        <input v-model="form.Phone" type="phonenum" id="form3Example4"
+                                            class="form-control" placeholder="Phone Number" />
+                                        <!-- <label class="form-label" for="form3Example3">Phone Number</label> -->
+                                    </div>
+
+                                    <!-- Password input -->
+                                    <div class="form-outline mb-4">
+                                        <input v-model="form.Password" type="password" id="form3Example5"
+                                            class="form-control" placeholder="Password" />
+                                        <!-- <label class="form-label" for="form3Example4">Password</label> -->
+                                    </div>
+
+                                    <div v-if="form.UserType === 'Consumer'">
+                                        <div class="col-md-12 mb-4">
+                                        <div class="form-outline">
+                                            <input v-model="form.Allergies" type="text" class="form-control" id="Allergies"
+                                                placeholder="Allergies (Eg. Shellfish)">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 mb-4">
+                                        <div class="form-outline">
+                                            <textarea  cols="4" rows="5" v-model="form.Fridge" type="text" id="Fridge" class="form-control"
+                                                placeholder="Items in your fridge (Eg. Banana)" />
+                                        </div><br>
+                                    </div>
+                                    </div>
+                                    <!-- Submit button -->
+                                    <div class="col-md-12 mb-4">
+                                        <div class="d-flex justify-content-between">
+                                            <button @click="goToNext" type="submit"
+                                                class="btn btn-success w-100">Submit</button>
+                                        </div>
+                                    </div>
+
+                                    <p style="text-align: center;">Already have an account? Click <router-link
+                                            :to="{ name: 'Login' }">here</router-link> to log in</p>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Jumbotron -->
+    </section>
     <!-- Section: Design Block -->
 </template>
-<script src="js/jquery.min.js"></script>
-<script src="js/popper.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
 <script>
 import axios from 'axios';
 
