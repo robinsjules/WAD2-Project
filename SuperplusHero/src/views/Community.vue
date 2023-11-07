adfb9075a0ae7363cf94e83c98bdfeb996a1f1f7
robinsjules <111500254+robinsjules@users.noreply.github.com>
2023-11-08 00:22:33 +0800

    community: added ratchet dropdwon but works

--- SuperplusHero/src/views/Community.vue
+++ SuperplusHero/src/views/Community.vue
@@ -35,6 +35,42 @@
     margin-left: 5px;
     margin-top: 3px;
 }
+
+.dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+.dropdown-toggle {
+    background-color: #f8f9fa;
+    border: 1px solid #ced4da;
+    padding: 8px 12px;
+    cursor: pointer;
+}
+
+.dropdown-menu {
+    display: none;
+    position: absolute;
+    background-color: #fff;
+    min-width: 160px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    z-index: 1;
+}
+
+.dropdown-menu a {
+    display: block;
+    padding: 8px 12px;
+    text-decoration: none;
+    color: #333;
+}
+
+.dropdown-menu a:hover {
+    background-color: #f5f5f5;
+}
+
+.dropdown-menu a:focus {
+    outline: none;
+}
 </style>
 
 <template>
@@ -45,10 +81,18 @@
                 <div class="form-group">
                     <div class="row justify-content-center">
                         <div class="input-group">
-                            <input v-model="searchQuery" type="text" class="form-control" placeholder="Search for post"> <!--Edit to be responsive-->
+                            <input v-model="searchQuery" type="text" class="form-control" placeholder="Search for post">
+                            <!--Edit to be responsive-->
                         </div>
                     </div>
                 </div>
+            </div>
+
+            <div class="form-group m-3">
+                <label for="sortOptions">Sort by: &nbsp;</label>
+                <select id="sortOptions" v-model="selectedSortOption" @change="sortPosts">
+                    <option v-for="option in sortOptions" :key="option">{{ option }}</option>
+                </select>
             </div>
 
             <div v-if="posts.length > 0">
@@ -79,7 +123,7 @@
                                             alt="Liked" class="heart-icon" />
                                         <img v-else @click="likePost(post)" src="../assets/heartNoFill.png" alt="Not Liked"
                                             class="heart-icon" />
-                                        <strong class="like-count">{{ post.Likes }} Likes</strong>
+                                        <strong class="like-count">{{ post.Likes }} likes</strong>
                                     </a>
                                 </small>
                             </div>
@@ -98,20 +142,23 @@
 
 <script>
 import axios from 'axios';
+import dropdown from 'vue-dropdowns';
 
 export default {
     data() {
         return {
             posts: [],
-            showFullText: [],
-            sortOptions: ['Newest', 'Oldest', 'Most Liked', 'Least Liked'], // add in frontend
+            sortOptions: ['Newest', 'Oldest', 'Most Liked'],
             selectedSortOption: 'Newest',
-            searchQuery: ''
+            searchQuery: '',
+            isDropdownOpen: false
         };
     },
+
     mounted() {
         this.fetchPostsFromServer();
     },
+
     watch: {
         searchQuery(newSearch) {
             if (newSearch === '') {
@@ -121,6 +168,11 @@ export default {
             }
         }
     },
+
+    components: {
+        'dropdown': dropdown,
+    },
+
     methods: {
         async fetchPostsFromServer() {
             try {
@@ -155,9 +207,22 @@ export default {
                 } else {
                     this.postsNotFound = false;
                 }
-
             } catch (error) {
                 console.error('Error searching posts:', error);
+            }
+        },
+
+        toggleDropdown() {
+            this.isDropdownOpen = !this.isDropdownOpen;
+        },
+
+        sortPosts() {
+            if (this.selectedSortOption === 'Newest' || this.selectedSortOption === 'Oldest') {
+                this.sortByCreatedAt(this.selectedSortOption);
+            } else if (this.selectedSortOption === 'Most Liked'
+            // || this.selectedSortOption === 'Least Liked'
+            ){
+                this.sortByLikes(this.selectedSortOption);
             }
         },
 
@@ -172,9 +237,10 @@ export default {
         sortByLikes(option) {
             if (option === 'Most Liked') {
                 this.posts.sort((a, b) => b.Likes - a.Likes);
-            } else if (option === 'Least Liked') {
-                this.posts.sort((a, b) => a.Likes - b.Likes);
             }
+            // else if (option === 'Least Liked') {
+            //     this.posts.sort((a, b) => a.Likes - b.Likes);
+            // }
         },
 
         async likePost(post) {
@@ -207,22 +273,8 @@ export default {
                     id: postId,
                     likes: updatedLikes
                 });
-
-                // if (response.data) {
-                //     // Update the post with the updated data from the server
-                //     const updatedPost = response.data;
-                //     const index = this.posts.findIndex(p => p.id === updatedPost.id);
-                //     if (index !== -1) {
-                //         this.posts[index] = updatedPost;
-                //     }
-                // }
             } catch (error) {
                 console.error('Error updating likes:', error);
-                // // Revert local changes on failure, if necessary
-                // if (post) {
-                //     post.Likes = updatedLikes;
-                //     post.liked = !post.liked;
-                // }
             }
         }
     },
